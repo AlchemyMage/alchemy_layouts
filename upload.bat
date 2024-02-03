6facf99f325c62ee4aea3b174a70bd31f1dcf95d
AlchemyMage <alchemy@posetmage.com>
2024-02-03 15:13:16 +0800

    upload

---
+++ upload.bat
@@ -0,0 +1,8 @@
+git config --local user.name "AlchemyMage"
+git config --local user.email "alchemy@posetmage.com"
+git remote set-url origin git@AM:AlchemyMage/alchemy_layouts.git
+
+git pull
+git add .
+git commit -m "upload"
+git push
